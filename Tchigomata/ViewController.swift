8ab65c082900061abdeb0578aaef45414f8f1a22
yl3506 <yl3506@nyu.edu>
2020-04-17 13:46:35 -0400

    Initial Commit

---
+++ Tchigomata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tchigomata
+//
+//  Created by Yichen on 4/17/20.
+//  Copyright © 2020 NYU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
